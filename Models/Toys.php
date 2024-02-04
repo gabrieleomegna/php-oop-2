352a941adfa3a5cfd346ec0734bbb976e1aa0f41
gabrieleomegna <gabrieleomegna01@gmail.com>
2024-02-05 00:06:34 +0100

    Corrected every file with trait

--- Models/Toys.php
+++ Models/Toys.php
@@ -6,8 +6,8 @@ class Toys extends Product {
     public $dimensions;
 
 
-    public function __construct(string $_name, string $_imageUrl, string $_description, string $_brand, Category $_category, string $_productType, float $_price, string $_materials, string $_dimensions) {
-        parent::__construct($_name, $_imageUrl, $_description, $_brand, $_category, $_productType, $_price);
+    public function __construct(string $_name, string $_imageUrl, string $_description, string $_brand, Category $_category, string $_productType, float $_price, string $hallway, string $shelf, int $shelfNumber, $_materials, string $_dimensions) {
+        parent::__construct($_name, $_imageUrl, $_description, $_brand, $_category, $_productType, $_price, $hallway, $shelf, $shelfNumber);
         $this->materials = $_materials;
         $this->dimensions = $_dimensions;
     }
